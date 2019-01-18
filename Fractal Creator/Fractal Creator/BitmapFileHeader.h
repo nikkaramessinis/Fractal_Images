1242c95d4de9d15d5602cb220191a19bac660c56
Nikos Karamessinis <nikkaramessinis@github.com>
2019-01-19 00:24:38 +0200

    Bitmap File Header

---
+++ Fractal Creator/Fractal Creator/BitmapFileHeader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+//makes c++ align all of this in 2 byte boundaries no hidden padding
+#pragma pack(2)
+
+using namespace std;
+
+struct BitmapFileHeader{
+	char header[2]{'B','M'};
+	int32_t fileSize;
+	int32_t reserved{ 0 };
+	int32_t dataOffset;
+};
